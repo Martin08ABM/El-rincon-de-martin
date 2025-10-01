f27df4b9578728e16f3fe194e3dba430947837b0
Martin08ABM <martin08abm@gmail.com>
2025-10-01 21:15:19 +0200

    Ajustada la altura máxima de las filas de las tablas de blog y projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // Importación del plugin de Tailwind CSS para Vite
 import tailwindcss from "@tailwindcss/vite";
 // Importación del adaptador de Vercel
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
