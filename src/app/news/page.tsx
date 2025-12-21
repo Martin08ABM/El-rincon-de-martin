5ff84261386a16fd896bdc8062f48c100443793b
Martin08ABM <martin08abm@gmail.com>
2025-12-21 21:57:22 +0100

    feat: Remove Filter components from Blog and News pages; add Footer to both pages; create About and Portfolio pages with content structure

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 // Importación de los componentes
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import Auth from '@/components/ui/auth';
 import NameLastname from '@/components/ui/nameLastname';
-import Filter from './components/Filter';
 
 // Metadata para la página del blog
 export const metadata: Metadata = {
@@ -27,18 +27,17 @@ export default async function BlogPage() {
   return (
     <div>
       <Header userData={<NameLastname />} auth={<Auth />} />
-      <div className='flex flex-row align-middle items-center justify-between mt-2 md:mt-4'>
+      <div className='flex flex-row align-middle items-center ml-8 mt-2 md:mt-4'>
         <h2 className="text-3xl font-bold mb-4 left-0">Noticias</h2>
-        <Filter />
       </div>
       <main className='flex flex-col relative py-2 -mt-4'>
         <div className='flex mt-8 mb-16 gap-8'>
           <ul className='grid grid-cols-4 gap-4 px-6 py-4 justify-center'>
             {posts?.map((post) => (
-              <li key={post.id} className='px-4 py-2 ml-4 mr-4 border-2 border-blue-700 rounded-xl bg-blue-600/40 hover:bg-blue-600/30 hover:scale-105 transition-all duration-200 shadow-xl shadow-blue-700/60'>
+              <li key={post.id} className='px-4 py-2 ml-10 mr-4 border-2 border-blue-700 rounded-xl bg-blue-600/40 hover:bg-blue-600/30 hover:scale-105 transition-all duration-200 shadow-xl shadow-blue-700/60 max-h-auto'>
                 <Link href={`/news/${post.slug}`}>
                   <h2 className='text-2xl font-black'>{post.data.title}</h2>
-                  <p className='text-lg font-normal'>{post.data.content}</p>
+                  <p className='text-lg font-normal max-h-36 overflow-hidden'>{post.data.content}</p>
                   <span className='text-[12px]'>{post.data.created_at}</span>
                 </Link>
               </li>
@@ -46,6 +45,7 @@ export default async function BlogPage() {
           </ul>
         </div>
       </main>
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center align-middle bg-transparent px-2 py-4 fixed bottom-0 w-full h-20">
+    <footer className="fixed bottom-0 left-0 right-0 flex justify-center items-center align-middle bg-transparent px-2 py-4 w-full h-20 z-10">
       <p className="text-xs md:text-md">&copy; {new Date().getFullYear()} Todos los derechos reservados</p>
       <div className="flex flex-row align-middle justify-center items-center ml-[10%] md:ml-[30%]">
         <p className="text-white text-xs md:text-md mr-4 hidden md:block">Redes sociales:</p>
         <div className="flex h-4 justify-between md:h-8">
           <ul className="flex flex-row decoration-none">
-            <li className="mr-4">
-              <Image src="/instagram.svg" width={4} height={4} alt="Instagram" className="w-4 h-4 md:w-8 md:h-8" />
-            </li>
             <li>
-              <Image src="/github.png" width={4} height={4} alt="Github" className="w-4 h-4 md:w-8 md:h-8 bg-white rounded-full border-none" />
-            </li>
-            <li className="ml-4 mr-4">
-              <Image src="/youtube.png" width={4} height={4} alt="Youtube" className="w-4 h-4 md:w-8 md:h-8" />
+              <Link href="https://github.com/Martin08ABM" target="_blank"><Image src="/github.png" width={4} height={4} alt="Github" className="w-4 h-4 md:w-8 md:h-8 bg-white rounded-full border-none" /></Link>
             </li>
           </ul>
         </div>
