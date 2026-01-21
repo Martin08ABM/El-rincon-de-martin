9029b1b34e76a11872f2b5f54c98bbc72ae4f30e
Martin08ABM <martin08abm@gmail.com>
2026-01-21 22:03:57 +0100

    Ajustes en el tamaño en el que se muestran las cards de las publicaciones

--- src/components/ui/blogSection.tsx
+++ src/components/ui/blogSection.tsx
@@ -13,10 +13,10 @@ export default async function BlogSection() {
   return (
     <div className="flex flex-col text-left mt-10 ml-4 md:ml-[50px] w-[90%] md:w-[80%]">
       <h2 className="text-base md:text-xl">Últimas publicaciones de mi blog:</h2>
-      <div className="flex flex-row left-0 relative mt-4 ml-2 md:ml-6 overflow-x-auto">
+      <div className="flex flex-col md:flex-row left-0 relative mt-4 ml-2 md:ml-6 overflow-x-auto">
         <ul className="flex flex-row gap-3 md:gap-4">
           {posts?.map((post) => (
-            <li key={post.id} className="min-w-[280px] md:max-w-[300px] max-h-[100px] shrink-0 overflow-hidden border-2 border-neutral-600 rounded-lg px-3 md:px-4 py-2 bg-neutral-800 hover:bg-neutral-700/40 transition-all duration-200">
+            <li key={post.id} className="max-w-lg md:max-w-xl max-h-lg md:max-h-2xl overflow-hidden border-2 border-neutral-600 rounded-lg px-3 md:px-4 py-2 bg-neutral-800 hover:bg-neutral-700/40 transition-all duration-200">
               <Link href={`/blog/${post.slug}`} className="text-blue-500">
                 <p className="text-base md:text-lg font-bold truncate">
                   {post.data.title}
